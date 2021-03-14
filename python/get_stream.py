179c7c2d82c040a27c77d74594925a52f3762b5d
NelsenEW <nelsenedbert@gmail.com>
2021-03-15 00:09:25 +0800

    Add comments and fix typo

--- python/get_stream.py
+++ python/get_stream.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from urllib.request import urlopen
 import os
@@ -7,33 +7,35 @@ import time
 import sys
 
 url="http://192.168.1.1:80"
-CAMERA_BUFFRER_SIZE=4096
+CAMERA_BUFFER_SIZE=4096
 stream=urlopen(url + "/stream.jpg")
 bts=b''
 i=0
-prev_time = time.perf_counter()
 
 while True:    
     try:
-        bts+=stream.read(CAMERA_BUFFRER_SIZE)
+        bts+=stream.read(CAMERA_BUFFER_SIZE)
         jpghead=bts.find(b'\xff\xd8')
         jpgend=bts.find(b'\xff\xd9')
         if jpghead>-1 and jpgend>-1:
             jpg=bts[jpghead:jpgend+2]
             bts=bts[jpgend+2:]
-            img=cv.imdecode(np.frombuffer(jpg,dtype=np.uint8),cv.IMREAD_UNCHANGED)
-            img=cv.resize(img,(640,480))
-            cv.imshow("a",img)
-        k=cv.waitKey(1)
+            img=cv2.imdecode(np.frombuffer(jpg,dtype=np.uint8),cv2.IMREAD_UNCHANGED)
+            img=cv2.resize(img,(640,480))
+            cv2.imshow("ESP32 CAM OPENCV stream",img)
+        k=cv2.waitKey(1)
     except Exception as e:
         print("Error:" + str(e))
         bts=b''
         stream=urlopen(url)
         continue
+    # Press 'a' to take a picture
     if k & 0xFF == ord('a'):
-        cv.imwrite(str(i) + ".jpg", img)
+        cv2.imwrite(str(i) + ".jpg", img)
+        print(f"Save image filename: {i}.jpg")
         i=i+1
+    # Press 'q' to quit
     if k & 0xFF == ord('q'):
         break
 
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
